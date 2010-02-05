040e7b2278bbbc3b4f3eb5fa931de550afb764d6
Shane Wolf <shanewolf@gmail.com>
2010-02-04 22:29:13 -0800

    speed tests

---
+++ spec/speed.rb
@@ -0,0 +1,36 @@
+require 'benchmark'
+require "#{File.dirname(__FILE__)}/../lib/lawnchair"
+
+Lawnchair.redis.flushdb
+
+# 
+# *** Performing 1000 iterations ***
+#              user       system        total        real
+# cached:     0.140000    0.040000    0.180000    ( 0.292324)
+# not cached: 26.070000   0.620000    26.690000   ( 27.156388)
+
+n = (ARGV.shift || 1000).to_i
+
+puts "*** Performing #{n} iterations ***"
+
+def expensive_stuff
+  100.times do
+    Date.parse("Dec 3. 1981")
+  end
+end
+
+Benchmark.bm(7) do |x|
+  x.report("cached:") do
+    (1..n).each do |i|
+      Lawnchair::Cache.please(:key => "foo") do
+        expensive_stuff
+      end
+    end
+  end
+  
+  x.report("not cached:") do
+    (1..n).each do |i|
+      expensive_stuff
+    end
+  end
+end
