3987f0076096079eb022f54d6d64753da4f42f67
Shane Wolf <shanewolf@gmail.com>
2010-02-14 19:27:28 -0800

    fix view helper

--- lib/view/helper.rb
+++ lib/view/helper.rb
@@ -3,7 +3,7 @@ module Lawnchair
   module View
     module Helper
       def lawnchair_cache(key, &block)
-        rendered_value = Lawnchair::Cache.me(key, :raw => true) do
+        rendered_value = Lawnchair.cache(key, :raw => true) do
           capture(&block)
         end
         concat(rendered_value)
